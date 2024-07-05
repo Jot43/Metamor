3dab29f28d1c468d07fedffe931f57c645d1af88
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-07-06 00:36:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+API_ID = int(os.environ.get("API_ID", "22353942"))
+API_HASH = os.environ.get("API_HASH", "a71c2aa4f6b764734411512973e34763")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7350355274:AAENQcdyg3-lqcAWDopEdRVEvcUWqhnu7QE")
+ADMIN = int(os.environ.get("ADMIN", '6643388068')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 group = environ.get('GROUP', '-1007350355274')
 GROUP = int(group) if group and id_pattern.search(group) else None
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+SUNRISES_PIC= "https://graph.org/file/12e81862346199c521ae8.jpg"  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '6643388068'))
 
 
 
