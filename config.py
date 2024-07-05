1f064376d7f7ede839056bc612acd07567f37135
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-07-05 22:42:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+API_ID = int(os.environ.get("API_ID", "22353942"))
+API_HASH = os.environ.get("API_HASH", "a71c2aa4f6b764734411512973e34763")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+ADMIN = int(os.environ.get("ADMIN", '6643388068')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "")
 CAPTION = os.environ.get("CAPTION", "")
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+#ALL FILES UPLOADED - CREDITS 🌟 - @
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '6643388068'))
 
 
 
